5db920717f894226ac1c766354cf1856202d252a
Swapnil kotwal <swapnil-kotwal-sp@users.noreply.github.com>
2020-09-09 20:10:18 +0530

    Update Untraced.java

--- my-demo/src/com/my/demo/Untraced.java
+++ my-demo/src/com/my/demo/Untraced.java
@@ -1,7 +1,7 @@
 package com.my.demo;
 
 /**
- * A marker annotation used by ConnectorTracingAspect to flag classes that should not
+ * A marker annotation used by TracingAspect to flag classes that should not
  * have the TracingAspect woven into them. Typically this is used either because
  * the class needs to be able to run in a constrained classpath (ie - without
  * the AspectJ jars) or to prevent recursion during tracing.
